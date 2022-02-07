0db7f9f0429a04b499f631784caeb11f4852c067
MiguelBarrosoVarela <mb3119@ic.ac.uk>
2022-02-07 20:55:49 +0000

    Added Market Saturation conditions to ensure simulation is stopped

--- Econophysics_Project/AverageBuySell.py
+++ Econophysics_Project/AverageBuySell.py
@@ -13,7 +13,7 @@ import random
 
 
 sig=3
-beta=0.02
+beta=3
 rho=1
 k=1
 m=2#Number of subgroups per group
@@ -40,22 +40,20 @@ def Hrarchy_Vect(i):
 #%%
 #Performing iterations
 S_vector_old=np.zeros(agents)
-Change_Times_List=[]
 t=0#Initialising time
-Finish=0
 times=np.array([])#List of times where agents buy
 sales=np.array([])#Sales at each time where agents buy
-sigmas=sig*np.ones(m**n)# List of sigma values for agents
-S_vector=np.zeros(m**n)#List of S values
+sigmas=sig*np.ones(agents)# List of sigma values for agents
+S_vector=np.zeros(agents)#List of S values
 Hrarchy_Sold_Bool=np.zeros((N,agents))
-Hrarchy_Sold_Numbers=np.zeros((N,agents))
+
 
 Split=0.5 # Approximate Starting Conditions: N_sellers/N_total
 
 for i in range(agents):
     Hrarchy_Sold_Bool[0][i]=np.sign(random.random()-1+Split)
 for i in range(1,N):
-    for j in range(m**(n-i)):
+    for j in range(m**(n-i)):  #average matrix 
             Hrarchy_Sold_Bool[i][j]=sum(Hrarchy_Sold_Bool[i-1][j*m+k] for k in range(m))/m
 
             
@@ -66,26 +64,24 @@ Need to initialise buy_time array - so first buy-time array
 """
 
 
-TimeArray=np.array([-np.log(1-random.random())/(k*sig**rho) for i in range(agents)]) #Generate times from distribution
-#%%
 
-while abs(Hrarchy_Sold_Bool[n][0])!=1:
-    ##############################################
-    
-    #This bit creates list of indices of agents who haven't sold
-    #This could be streamlined with new boost method, but we'll do that later. 
-    
-    # UnsoldAgntsInds=np.array([],dtype=int)
-    # for i in range(m**n):
-    #     if Hrarchy_Sold_Bool[0][i]==0:
-    #         UnsoldAgntsInds=np.append(UnsoldAgntsInds,i)
+TimeArray=np.array([-np.log(1-random.random())/(k*sig**rho) for i in range(agents)]) #Generate times from distribution
+t,buyer=Boost(TimeArray)
+t_old=t
+#%%
+BuyingPercentage=0.9 #Choose when the market is considered to be dominated
+
+while abs(Hrarchy_Sold_Bool[n][0])<BuyingPercentage:
+   
             
     t,buyer=Boost(TimeArray)  #Finds smallest time and the buyer index corresponding to that time
+    if t/t_old>10:
+        break #stops if the next time to buy is super super far (market dominated)
+    t_old=t  
     
+    Hrarchy_Sold_Bool[0][buyer]*=-1 #Flip the buyer/seller to seller/buyer
     
-    Hrarchy_Sold_Bool[0][buyer]*=-1
-    
-    #Getting boolean Hrarchy matrix from numerical matrix
+    #Getting averages matrix
     for i in range(1,N):
         for j in range(m**(n-i)):
             Hrarchy_Sold_Bool[i][j]=sum(Hrarchy_Sold_Bool[i-1][j*m+k] for k in range(m))/m
@@ -99,22 +95,7 @@ while abs(Hrarchy_Sold_Bool[n][0])!=1:
     #This calculates new_sigmas
         sign=np.sign(Hrarchy_Sold_Bool[0][i]) #determine which type of agent it is
         sigmas[i]=sig**rho*2**(-sign*S_vector[i]*beta)    
-    
-    """
-    Generate new buy times
-    ----------------------
-    
-    S_vector is the list of S values for the agents. E.g. S_vector[3] is the S
-    value for the agent with index 3. Perhaps store the previous s vector and 
-    compare this to the current S_vector. 
-    
-    For the agents with changed S_vector values, generate new times. Use the 
-    PDF to generate dt. Then add dt to t (which is the time of this iteration)
-    - this is their new buy time. 
 
-    
-
-    """
     
     for j in range(len(TimeArray)):
         if S_vector[j]==S_vector_old[j]:  #Does what is described above
@@ -133,15 +114,13 @@ while abs(Hrarchy_Sold_Bool[n][0])!=1:
     plt.show()                     #in case you want to animate the market
     plt.pause(0.0000001)             #this makes code slower of course
     S_vector_old=S_vector 
-    #make sure this buyer never gets picked again by boost method
-  
-    #t+=dt, assumed this bit is no longer needed as no longer using FD method
+
 #%%        
 plt.figure(0)
 plt.xlabel(r'Non-dimensional time ',size=15)
 plt.ylabel('Buyers',size=15) 
 
-plt.plot(times,sales,label=r'$\sigma^{\rho}=$'+str(sig)+ r' , $\beta$= '+str(beta)+', N='+str(m**n))
+plt.plot(times,sales,label=r'$\sigma^{\rho}=$'+str(sig)+ r' , $\beta$= '+str(beta)+', N='+str(m**n)+r', Sold/Bought Fraction='+str(BuyingPercentage))
 plt.legend()    
         
     
