0daafb35da2bbb27cb6e114ff777ea4b899e3917
MiguelBarrosoVarela <mb3119@ic.ac.uk>
2022-03-21 20:33:40 +0000

    CriticalTime VS Beta created

--- Econophysics_Project/CriticalTimeVSBeta.py
+++ Econophysics_Project/CriticalTimeVSBeta.py
@@ -7,10 +7,30 @@ import Buy_only_simulation as BOS
 import matplotlib.pyplot as plt
 import numpy as np
 
-CriticalTimes=[]
-for i in np.linspace(1,6,100):
-    times,sales=BOS.BuyOnlySimulation(2,10,2,i)
-    CriticalTimes.append(times[-1])
-    print(i)
 
-plt.plot(np.linspace(1,6,0.05),CriticalTimes)
+BetaRange=np.linspace(0.5,2,30)
+CriticalTimes=[[] for i in range(3)]
+count=0
+for j in range(2,5,1):
+ 
+ for i in BetaRange:
+    times,sales=BOS.BuyOnlySimulation(m=j,N=9,lam=2,beta=i)
+    CriticalTimes[count].append(times[-1])
+    print(i)
+ count+=1
+ print(j)   
+#%%
+for j in range(3):   
+    plt.figure(1)
+    plt.xlabel(r'$\beta$',size=15)
+    plt.ylabel('Critical Time',size=15)
+    plt.plot(BetaRange,CriticalTimes[j],label='N=8,'+'m='+str(j+2))
+plt.legend(prop={"size":11})
+#%%
+import scipy.special as sc
+
+def Criti(beta,m=3):
+    return(1/2 * 2**(-m*beta) * sc.expi( np.log(2)*beta*m ))
+
+plt.plot(BetaRange,Criti(beta=BetaRange,m=2),label='Analytical Critical Time for m=2')
+
